6e6639a7a0dc3b5762b56961d2a29e9932946033
Md. Ridoy Islam <md.ridoyislam@Mds-MacBook-Pro.local>
2024-01-18 21:00:15 +0600

    deploy changes

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import Cookies from "universal-cookie";
 // import { cookies } from "next/headers";
 // const API_URL = process.env.NEXT_PUBLIC_API;
-const API_URL = "http://217.196.50.52:5000/api";
+const API_URL = "https://api.robofxtrader.com/api";
 // const API_URL = "http://localhost:3001/api";
 // const cookie = new Cookies();
 // const token = cookie.get("jwt");
