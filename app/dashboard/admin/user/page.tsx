b5fc9eb2fdb0405d28f9accd7a80d777dfe1f3cf
Shahadat Hossen <shahadat.07.sh@gmail.com>
2024-01-18 12:29:20 +0600

    image upload added

--- app/dashboard/admin/user/page.tsx
+++ app/dashboard/admin/user/page.tsx
@@ -4,6 +4,7 @@ import getAllUsers from "@/app/actions/getAllUsers";
 
 const page = async () => {
   const allUsers = await getAllUsers();
+  // console.log("all users", allUsers);
   return (
     <div>
       <User allUsers={allUsers?.data} />
--- utils/axios.ts
+++ utils/axios.ts
@@ -1,31 +1,25 @@
-import axios, { AxiosInstance } from "axios";
-// import cookie from "js-cookie";
+"use client";
 
-const API_URL = process.env.NEXT_PUBLIC_API;
+import axios, { AxiosInstance } from "axios";
+import Cookies from "universal-cookie";
+// import { cookies } from "next/headers";
+// const API_URL = process.env.NEXT_PUBLIC_API;
 // const API_URL = "http://217.196.50.52:5000/api";
-//const API_URL = "http://localhost:3001/api";
+const API_URL = "http://localhost:3001/api";
+// const cookie = new Cookies();
+// const token = cookie.get("jwt");
+// console.log("here is the token", token);
 
 const Axios: AxiosInstance = axios.create({
   baseURL: API_URL,
-  // timeout: 1000,
 });
 
-// Add an interceptor to include the authorization token in the headers
 // Axios.interceptors.request.use(
 //   (config) => {
-//     const token = cookie.get("jwt");
-//     console.log("token", token);
-
-//     // If a token is available, add it to the headers
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//       // config.headers.Authorization = `Bearer ${JWT}`;
-//     }
-
+//     config.headers["Authorization"] = `Bearer ${token}`;
 //     return config;
 //   },
 //   (error) => {
-//     // Handle request error
 //     return Promise.reject(error);
 //   }
 // );
